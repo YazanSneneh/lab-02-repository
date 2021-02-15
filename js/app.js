407bd751dff1d83ca0aae03c3789cddb3e703cba
Boshra Jaber <boshrajaber94@gmail.com>
2021-02-15 15:39:35 +0200

    created constractor and render method

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+
+function Images(image_url, title, description, keyword, horns) {
+    this.image = image_url;
+    this.titleImg = title;
+    this.descriptionImg = description;
+    this.keywordImg = keyword;
+    this.hornsImg = horns;
+}
+
+Images.prototype.render = function () {
+    let $imageClone = $('#photo-template').clone();
+    $('main').append($imageClone);
+
+    $imageClone.find('h2').text(this.titleImg);
+    $imageClone.find('img').attr('src', this.image);
+    $imageClone.find('p').text(this.descriptionImg);
+
+}// create a method that each time it will add an item to the object/ constractor
+// first: do JSON quest.
+// 
+function readJson() {
+    const ajaxSettings = {
+        method: 'get',
+        dataType: 'json'
+    };
+
+    $.ajax('page-1.json', ajaxSettings)
+        .then(data => {
+            data.forEach(element => {
+                let newImage = new Images(element.image_url, element.title, element.description);
+                newImage.render();
+            });
+        });
+}
+
+$('document').ready(readJson);
