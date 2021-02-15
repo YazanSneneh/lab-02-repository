1189ca89b047de28c7be84584495125f238dbe1b
yazan <yaz.sneneh@gmail.com>
2021-02-15 20:04:13 +0200

    add css and filter features

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+let keywordsArray = []
 function Images(image_url, title, description, keyword, horns) {
     this.image = image_url;
     this.titleImg = title;
@@ -14,7 +14,7 @@ Images.prototype.render = function () {
     $imageClone.find('h2').text(this.titleImg);
     $imageClone.find('img').attr('src', this.image);
     $imageClone.find('p').text(this.descriptionImg);
-
+    $imageClone.removeClass('remove-template');
 
 }// create a method that each time it will add an item to the object/ constractor
 // first: do JSON quest.
@@ -28,9 +28,45 @@ function readJson() {
         .then(data => {
             data.forEach(element => {
                 let newImage = new Images(element.image_url, element.title, element.description, element.keyword, element.horns);
+                keywordsArray.push(element.keyword);
                 newImage.render();
             });
+            makeDropDownList(keywordsArray)
         });
 }
 
-$(document).ready(readJson);
+function makeDropDownList(keywords){
+    keywords.forEach(item =>{
+        let option = $('<option> </option>').text(item);
+        $('#drop-down').append(option);
+    })
+}
+function displayFiltered(e){
+    let keyword = e.target.value;
+    alert(keyword)
+}
+
+// function filt(item){
+//     let result = [];
+//     if(result.length ===0){
+//         result.push(item);
+//     }
+//     for(let i=0; i<result.length; i++){
+//         if(item !== result[i]){
+//             result.push(item)
+//         }
+//     }
+// }
+$(document).ready(() => {
+    readJson();
+    $('#drop-down').on('change',displayFiltered)
+    
+});
+
+ /*
+ 2. render elements in the dropdown list.
+       select element from html
+       create option for each element in the array
+       append it to selec element
+    
+ */
