b355bc7f136d6390f8620bfbc45158ae12ddf435
Boshra Jaber <boshrajaber94@gmail.com>
2021-02-16 17:17:05 +0200

    1

--- js/todolist.js
+++ js/todolist.js
@@ -1,7 +1,19 @@
-// 1. flitering
+// 
 // 2. add lintercode
 // 3. Feature 1: Pagination
 //        2 buttons, readjson from file2 
 // 4. Feature 2: Templating
 // 5. Feature 3: Styling with Flexbox 
 // 6. Feature 4: Sort the image
+
+
+function sortingImages(arrayOfImages){
+    if( a.title.toUpperCase() > b.title.toUpperCase()){
+        return -1;
+    } else if ( a.title.toUpperCase() < b.title.toUpperCase()){
+        return 1;
+    }
+    return 0;
+}
+
+
